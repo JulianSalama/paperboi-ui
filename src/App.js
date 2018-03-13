ce02d5022a141890fcfeb3e355939c0eba814c7b
jsalama <juliansalama@gmail.com>
2018-03-13 17:22:57 -0400

    prod deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { AccountDatasetList, AccountDatasetCreate } from './account_datasets';
 import Dashboard from './dashboard';
 
 const App = () => (
-    <Admin dashboard={Dashboard} restClient={jsonServerRestClient('http://localhost:8080')}>
+    <Admin dashboard={Dashboard} restClient={jsonServerRestClient('http:paperboi-dev.us-east-1.elasticbeanstalk.com')}>
         <Resource name="account_databases" create={AccountDatabaseCreate} list={AccountDatabasesList} />
         <Resource name="account_datasets" create={AccountDatasetCreate} list={AccountDatasetList} />
     </Admin>
